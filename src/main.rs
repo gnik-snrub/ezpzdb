8d7bcc59e98ece574198670372955da0e2639da7
Josiah Morris <josiahbmorris@hotmail.com>
2025-03-05 03:02:56 +0900

    Refactored to use serde_json::Value, rather than String, for the HashMap value

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::{Parser, Subcommand};
+use std::fs;
 use std::collections::HashMap;
+use serde_json::{self, Value, json};
 
 // Simple key-value store CLI
 #[derive(Parser, Debug)]
@@ -26,20 +28,29 @@ enum Command {
     },
 }
 
-fn save(store: &HashMap<String, String>) {
-    let store_bin = bincode::serialize(&store).unwrap();
+fn save(store: &HashMap<String, Value>) {
+    let store_bin = serde_json::to_string(&store).unwrap();
 
-    std::fs::write("store.bin", store_bin).unwrap();
+    fs::write("store.bin", store_bin).unwrap();
 }
 
-fn load() -> HashMap<String, String> {
-    let store_bin = std::fs::read("store.bin").unwrap();
-    bincode::deserialize(&store_bin).unwrap()
+fn load() -> HashMap<String, Value> {
+    let store_bin = fs::read_to_string("store.bin");
+    match store_bin {
+        Ok(json_str) => serde_json::from_str(&json_str).unwrap_or_else(|e| {
+            eprintln!("Failed to parse JSON: {}", e);
+            HashMap::new()
+        }),
+        Err(e) => {
+            eprintln!("Failed to read file: {}", e);
+            HashMap::new()
+        }
+    }
 }
 
 fn main() {
     let cli = Cli::parse();
-    let mut store: HashMap<String, String> = load();
+    let mut store: HashMap<String, Value> = load();
 
     match cli {
         Cli { command: Some(Command::Read) } => {
