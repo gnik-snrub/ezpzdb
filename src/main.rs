8674b5f1d13f6adad5e2f1556c84f1529b045c33
Josiah Morris <josiahbmorris@hotmail.com>
2025-03-13 03:59:14 +0900

    Built WhereClause parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use clap::{Parser, Subcommand};
-use std::fs;
+use core::num;
+use std::any::type_name;
+use std::string;
+use std::{any::Any, fs};
 use std::collections::HashMap;
-use serde_json::{self, Value, json};
+use serde_json::{self, json, Number, Value};
 
 // Simple key-value store CLI
 #[derive(Parser, Debug)]
@@ -112,45 +115,68 @@ fn main() {
         Cli { command: Some(Command::Search { query }) } => {
             let mut select_tokens = vec![];
             let mut from_tokens = vec![];
-            let mut where_tokens = vec![];
+            let mut where_tokens: Vec<WhereClause> = vec![];
 
-            let mut current_token = CurrentToken::None;
+            let mut temp_where_tokens: Vec<String> = vec![];
+
+            let mut current_token: TokenOption = TokenOption::CurrentToken(CurrentToken::None);
 
             for q in query {
-                if q == "SELECT" {
-                    current_token = CurrentToken::Select;
-                } else if q == "FROM" {
-                    current_token = CurrentToken::From;
-                } else if q == "WHERE" {
-                    current_token = CurrentToken::Where;
-                } else {
-                    match current_token {
-                        CurrentToken::Select => select_tokens.push(q),
-                        CurrentToken::From => from_tokens.push(q),
-                        CurrentToken::Where => where_tokens.push(q),
-                        CurrentToken::None => {}
+                if temp_where_tokens.len() > 3 {
+                    finalize_where_clause(&mut temp_where_tokens, &mut where_tokens);
+                } else if temp_where_tokens.len() > 2 && !(temp_where_tokens.contains(&String::from("AND")) || temp_where_tokens.contains(&String::from("OR"))) {
+                    finalize_where_clause(&mut temp_where_tokens, &mut where_tokens);
+                }
+                match q.as_str() {
+                    "SELECT" => current_token = TokenOption::CurrentToken(CurrentToken::Select),
+                    "FROM" => current_token = TokenOption::CurrentToken(CurrentToken::From),
+                    "WHERE" => current_token = TokenOption::CurrentToken(CurrentToken::Where),
+                    "AND" => {
+                        finalize_where_clause(&mut temp_where_tokens, &mut where_tokens);
+                        temp_where_tokens.push(q);
+                    },
+                    "OR" => {
+                        finalize_where_clause(&mut temp_where_tokens, &mut where_tokens);
+                        temp_where_tokens.push(q);
+                    }
+                    _ => {
+                        match current_token {
+                            TokenOption::CurrentToken(CurrentToken::Select) => select_tokens.push(q),
+                            TokenOption::CurrentToken(CurrentToken::From) => from_tokens.push(q),
+                            TokenOption::CurrentToken(CurrentToken::Where) => temp_where_tokens.push(q),
+                            _ => {}
+                        }
                     }
                 }
             }
+            finalize_where_clause(&mut temp_where_tokens, &mut where_tokens);
 
-            println!("Select: {:?}, From: {:?}, Where: {:?}", select_tokens, from_tokens, where_tokens);
             let query = Query {
                 select: select_tokens,
                 from: from_tokens.join(""),
-                where_clause: match where_tokens[1].as_str() {
-                    "=" => Some(Condition::Equals(where_tokens[0].to_string(), SearchValue::Text(where_tokens[2].to_string()))),
-                    "!=" => Some(Condition::NotEquals(where_tokens[0].to_string(), SearchValue::Text(where_tokens[2].to_string()))),
-                    ">" => Some(Condition::GreaterThan(where_tokens[0].to_string(), SearchValue::Text(where_tokens[2].to_string()))),
-                    "<" => Some(Condition::LessThan(where_tokens[0].to_string(), SearchValue::Text(where_tokens[2].to_string()))),
-                    _ => None
+                where_clause: match where_tokens.len() {
+                    0 => None,
+                    _ => Some(where_tokens),
                 }
             };
-            println!("Query: {:?}", query);
+            println!("{:?}", query);
         }
         _ => {
             println!("No command provided");
         }
     }
+}
+
+fn finalize_where_clause(temp_where_tokens: &mut Vec<String>, where_tokens: &mut Vec<WhereClause>) {
+    if !temp_where_tokens.is_empty() {
+        where_tokens.push(build_where_clause(temp_where_tokens.clone()));
+        temp_where_tokens.clear();
+    }
+}
+
+enum TokenOption {
+    CurrentToken(CurrentToken),
+    Connector(Connector),
 }
 
 enum CurrentToken {
@@ -164,21 +190,65 @@ enum CurrentToken {
 struct Query {
     select: Vec<String>,
     from: String,
-    where_clause: Option<Condition>
+    where_clause: Option<Vec<WhereClause>>,
+}
+
+fn build_where_clause(mut where_tokens: Vec<String>) -> WhereClause {
+    let connector = if (where_tokens[0] == String::from("AND")) || (where_tokens[0] == String::from("OR")) {
+        let found = where_tokens.remove(0);
+        match found.as_str() {
+            "AND" => Some(Connector::AND),
+            _ => Some(Connector::OR)
+        }
+    } else {
+        None
+    };
+    WhereClause {
+        left_hand: where_tokens.get(0).cloned().unwrap_or_default(),
+        operator: match where_tokens.get(1).map(|s| s.as_str()) {
+            Some("=") => Condition::Equals,
+            Some("!=") => Condition::NotEquals,
+            Some(">") => Condition::GreaterThan,
+            Some("<") => Condition::LessThan,
+            _ => Condition::Invalid
+        },
+        right_hand: match where_tokens.get(2) {
+            Some(t) if t.parse::<i32>().is_ok() => RightHandType::Integer(i32::from(t.parse::<i32>().unwrap())),
+            Some(t) if t.parse::<f64>().is_ok() => RightHandType::Float(f64::from(t.parse::<f64>().unwrap())),
+            Some(t) if t.parse::<bool>().is_ok() => RightHandType::Boolean(bool::from(t.parse::<bool>().unwrap())),
+            _ => {RightHandType::String(where_tokens[2].clone())},
+        },
+        connector,
+    }
+}
+
+#[derive(Debug)]
+struct WhereClause {
+    left_hand: String,
+    operator: Condition,
+    right_hand: RightHandType,
+    connector: Option<Connector>,
+}
+
+#[derive(Debug)]
+enum Connector {
+    AND,
+    OR,
+}
+
+#[derive(Debug)]
+enum RightHandType {
+    String(String),
+    Integer(i32),
+    Float(f64),
+    Boolean(bool),
 }
 
 #[derive(Debug)]
 enum Condition {
-    Equals(String, SearchValue),
-    NotEquals(String, SearchValue),
-    GreaterThan(String, SearchValue),
-    LessThan(String, SearchValue),
-}
-
-#[derive(Debug)]
-enum SearchValue {
-    Integer(i32),
-    Float(f64),
-    Text(String),
-    Boolean(bool),
+    Equals,
+    NotEquals,
+    GreaterThan,
+    LessThan,
+    Invalid
 }
