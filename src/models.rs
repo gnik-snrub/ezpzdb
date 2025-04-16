1be137503cb76307cff17deeb87715dc5b1f5fb8
Josiah Morris <josiahbmorris@hotmail.com>
2025-04-17 04:39:49 +0900

    Added Index struct, and support structs

--- src/models.rs
+++ src/models.rs
@@ -1,14 +1,56 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use serde_json::{Number, Value};
 
+use std::cmp::Ordering;
+use ordered_float::OrderedFloat;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Table {
     pub name: String,
     pub schema: Vec<FieldDef>,
     pub data: HashMap<Value, Value>,
+    pub indexes: HashMap<String, Index>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Index {
+    pub name: String,
+    pub field_name: String,
+    pub index_type: FieldDataType,
+    pub index_data: IndexStore,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum IndexStore {
+    Text(BTreeMap<String, Vec<Value>>),
+    Number(BTreeMap<IndexNumber, Vec<Value>>),
+    Boolean(BTreeMap<bool, Vec<Value>>),
+}
+
+pub enum IndexNumber {
+    Int(i64),
+    Float(OrderedFloat<f64>),
+}
+
+impl Eq for IndexNumber {}
+
+impl Ord for IndexNumber {
+    fn cmp(&self, other: &self) -> Ordering {
+        match (self, other) {
+            (IndexNumber::Int(a), IndexNumber::Int(b)) => a.cmp(b),
+            (IndexNumber::Float(a), IndexNumber::Float(b)) => a.cmp(b),
+            (IndexNumber::Int(a), IndexNumber::Float(b)) => OrderedFloat(*a as f64).cmp(b),
+            (IndexNumber::Float(a), IndexNumber::Int(b)) => a.cmp(&OrderedFloat(*b as f64)),
+        }
+    }
+}
+
+impl PartialOrd for IndexNumber {
+    fn partial_cmp(&self, other: &self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
